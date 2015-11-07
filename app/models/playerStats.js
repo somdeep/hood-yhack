e04c99c23ba0f1677c3e3667e5d1254f2b5f6b85
Harshit Saxena <hs2873@columbia.edu>
2015-11-07 03:29:19 -0500

    adding Player Stats model

--- app/models/playerStats.js
+++ app/models/playerStats.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var dbSchema = new Schema({
-		});
+		},{strict:false});
 
 module.exports = mongoose.model('playerStats', dbSchema);

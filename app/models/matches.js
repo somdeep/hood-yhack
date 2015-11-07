89f1b68426673f3c8381134a2549be9ebd1d43a9
Devwrat More <devwratmmore@gmail.com>
2015-11-07 14:20:52 -0500

    All schemas added

--- app/models/matches.js
+++ app/models/matches.js
@@ -2,7 +2,20 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var dbSchema = new Schema({
-		},{strict:false});
+var dbSchema = new Schema(
+    {
+        id: String,
+        team1Id: String,
+        team2Id: String,
+        date: Date,
+        place: String,
+        team1Score: Number,
+        team2Score: Number,
+        photos: Array,
+        mvp1Id: String,
+        mvp2Id: String
+    },
+    { strict: false}
+);
 
 module.exports = mongoose.model('matches', dbSchema);
--- app/models/playerProfile.js
+++ app/models/playerProfile.js
@@ -3,8 +3,22 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var dbSchema = new Schema({
-		},{strict:false});
+        name: String,
+        teamId: String,
+        nickName: String,
+        position: String,
+        jersey: String,
+        hood: String,
+        stats:
+            {
+                points: Number,
+                rebounds: Number,
+                assists: Number,
+                steals: Number,
+                mvpCount: Number,
+            }
+    },
+    { strict: false }
+);
 
 module.exports = mongoose.model('playerProfile', dbSchema);
-
-

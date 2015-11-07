ceaab3b0772283793c83a3dafe67c7c07f464ffc
Harshit Saxena <harshitsaxena@Harshits-MacBook-Pro.local>
2015-11-07 03:02:12 -0500

    CRUD for player done

--- app/models/playerProfile.js
+++ app/models/playerProfile.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var dbSchema = new Schema({
-		});
+		},{strict:false});
 
 module.exports = mongoose.model('playerProfile', dbSchema);
+
+
--- app/routes/playerRoutes.js
+++ app/routes/playerRoutes.js
@@ -2,8 +2,76 @@ var mongoose = require('mongoose');
 
 var playerProfile = require('./../models/playerProfile');
 
+var playerSchema = {
+name: 1,
+player_id:1,
+address: 1,
+contact: 1
+};
+
 module.exports = function(app) {
-    app.get('/player', function(req,res) {
-        res.json({'success':'player yayyyy'});
+
+	app.get('/player/get', function (req,res) {
+
+        playerProfile.find(function (err,players){
+        	if(err)
+        		res.send(err);
+        	res.json(players);
+        });
+    });
+
+
+
+	app.get('/player/get/:player_id', function (req,res){
+
+    	playerProfile.find({player_id:req.params.player_id}, function (err,data){
+    		if(err)
+    			res.send(err);
+    		res.json(data);
+    	});
+    });
+
+
+    app.post('/player/create', function(req,res){
+
+    	var player = req.body;
+    	//player.name=req.body.name;
+    	console.log(player);
+    	playerProfile.create(player,function(err,data){
+
+    		if(err)
+        		res.send(err);
+
+        	res.json(data);
+
+
+    	});
+
+
+    });
+
+    app.put('/player/update/:player_id', function (req,res){
+    	console.log(req.body);
+
+    	var updated=req.body;
+    	console.log(updated);
+
+    	playerProfile.update ({player_id:req.params.player_id},{$set : updated}, function (err,updated){
+    		if (err) 
+    			res.send(err);
+    			
+    			res.json(updated);
+    		});
+    	});
+
+    
+
+    
+    app.delete('/player/delete/:player_id', function (req,res){
+    	playerProfile.remove({player_id:req.params.player_id},function (err,removed){
+    		if(err)
+    			res.send(err);
+    		res.json(removed);
+    	});
     });
 };

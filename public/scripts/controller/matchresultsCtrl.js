e4e64b80cc270443865c827e30b4ec3105168731
Harshit Saxena <hs2873@columbia.edu>
2015-11-08 02:26:12 -0500

    Match fixed

--- public/scripts/controller/matchresultsCtrl.js
+++ public/scripts/controller/matchresultsCtrl.js
@@ -1,4 +1,8 @@
 
 app.controller('matchresultsCtrl', function ($scope,$http,$location) {
-	console.log('Match control is under control :P ');
+	
+  console.log('Match control is under ');
+        $scope.matchData = [{"teamnames":"points"},{"work":"try"}] ;
+       
+
 });

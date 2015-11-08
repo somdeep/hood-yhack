c66f4b1fc374baba1a589ac32c1473c121e09e7d
Devwrat More <devwratmmore@gmail.com>
2015-11-08 08:04:50 -0500

    server.js change

--- server.js
+++ server.js
@@ -2,15 +2,13 @@
 //Initial configuration
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
-var bodyParser     = require('body-parser');			// To fetch data during posts
 
-
-var port  	 = process.env.PORT || 8000; 				// set the port
 var bodyParser     = require('body-parser');			// To fetch data during posts
 var port  	 = process.env.PORT || 8000; 				// set the port
 // auth related
-var session      = require('express-session');
 
+var database = require('./app/config/database');
+var session      = require('express-session');
 
 //Middle-tier configuration
 
@@ -18,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-ww
 app.use(bodyParser.json())    // parse application/json
 
 app.use(express.static(__dirname + '/public')); 	// set the static files location
-app.use(express.static(__dirname + '/tmp')); 	// set the static files location
 
 require('./app/routes/routes.js')(app);
 

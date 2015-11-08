abe4d0b28cbca1b12060050814028f3a39df0317
Devwrat More <devwratmmore@gmail.com>
2015-11-07 19:23:32 -0500

    adding bodyparser

--- server.js
+++ server.js
@@ -2,8 +2,21 @@
 //Initial configuration
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
+var bodyParser     = require('body-parser');			// To fetch data during posts
+
 
 var port  	 = process.env.PORT || 8000; 				// set the port
+var bodyParser     = require('body-parser');			// To fetch data during posts
+var database = require('./app/config/database'); 			// load the database config
+
+// auth related
+var session      = require('express-session');
+
+
+//Middle-tier configuration
+
+app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-www-form-urlencoded
+app.use(bodyParser.json())    // parse application/json
 
 app.use(express.static(__dirname + '/public')); 	// set the static files location
 

5a8bee9a2b4dcb544b03ded34b2b39f28f7b9e30
Devwrat More <devwratmmore@gmail.com>
2015-11-07 19:07:25 -0500

    Changing port in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 var bodyParser     = require('body-parser');			// To fetch data during posts
-var port  	 = process.env.PORT || 8000; 				// set the port
+var port  	 = process.env.PORT || 1337; 				// set the port
 var database = require('./app/config/database'); 			// load the database config
 
 var db = mongoose.connect(database.url);	// connect to mongoDB database on modulus.io

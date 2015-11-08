b79a8fed4c486c75f0df5806367fd07516ecf61f
Devwrat More <devwratmmore@gmail.com>
2015-11-08 07:09:01 -0500

    Adding elo ratings to teams

--- python/generateTestData.py
+++ python/generateTestData.py
@@ -16,28 +16,18 @@ def updatePlayerStats(players):
     score = 0
     for p in players:
         #print "**********************************"
-        #print "P ", p
         playerCursor = db.playerprofiles.find({"playerId":str(p)})
         for cursor in playerCursor:
             pp = cursor;
-        #print "Retrieved player\n", pp
         points = pp['stats']['points']
-        #print "previous points", points
         points += rnd.randint(0, 40)
-        #print "points", points
         score += points
         rebounds = pp['stats']['rebounds']
-        #print "previous rebounds", rebounds
         rebounds += rnd.randint(0, 22)
-        #print "rebounds", rebounds
         assists = pp['stats']['assists']
-        #print "previous assists", assists
         assists += rnd.randint(0, 18)
-        #print "assists", assists
         steals = pp['stats']['steals']
-        #print "previous steals", steals
         steals += rnd.randint(0, 7)
-        #print "steals", steals
 
         result = db.playerprofiles.update_one(
                 { "playerId": str(p) },
@@ -45,13 +35,9 @@ def updatePlayerStats(players):
                     "$set": {"stats.points": points, "stats.rebounds":rebounds, "stats.assists":assists, "stats.steals":steals}
                 }
             )
-        #print "matched results", result.matched_count
-        #print "modified results", result.modified_count
-        #print "P ", p
         playerCursor = db.playerprofiles.find({"playerId":str(p)})
         for cursor in playerCursor:
             pp = cursor;
-        #print "Retrieved player\n", pp
 
     return score
 
@@ -61,16 +47,11 @@ teamCount = 100
 totalPlayers = 0
 minPlayerIndex = 1
 for teamId in range(1, teamCount+1):
-    ###print "teamId", teamId
     playerCount = rnd.randint(5, 10)
-    ##print "playerCount = ", playerCount
 
     totalPlayers += playerCount
-    ###print "playercount ", playerCount
-    #teamMembers = range((teamId-1) * playerCount + 1, teamId * playerCount + 1)
     teamMembers = range(minPlayerIndex, minPlayerIndex + playerCount)
     minPlayerIndex = minPlayerIndex + playerCount
-    ##print "teamMembers : ", teamMembers
     hood = hoods[rnd.randint(0, 24)]
     for player in teamMembers:
         result = db.playerprofiles.insert_one(
@@ -100,12 +81,12 @@ for teamId in range(1, teamCount+1):
                 "stats" :
                     {
                         "won": 0,
-                        "lost": 0
+                        "lost": 0,
+                        "rating": 1300.00,
                     }
                 }
             )
 
-###print totalPlayers
 
 ##print "############################### MATCH INSERTION BEGINS ###################"
 
@@ -114,9 +95,7 @@ matchCount = 2000;
 
 for matchId in range(1, matchCount+1):
     team1Id = rnd.randint(1, teamCount)
-    ###print "id1", team1Id
     team2Id = rnd.randint(1, teamCount)
-    ###print "id2", team2Id
     while team2Id == team1Id:
         team2Id = rnd.randint(1, teamCount)
 
@@ -124,15 +103,11 @@ for matchId in range(1, matchCount+1):
     team2Cursor = db.teamprofiles.find({"teamId":str(team2Id)})
     for t in team1Cursor:
         team1 = t
-    ##print "team1\n", team1
     players1 = team1['teamMembers']
-    ##print "players1\n", players1
 
     for t in team2Cursor:
         team2 = t
-    ##print "team2\n", team2
     players2 = team2['teamMembers']
-    ##print "players1\n", players1
 
     player1Stats = {}
     player2Stats = {}
@@ -141,12 +116,8 @@ for matchId in range(1, matchCount+1):
 
 
     mvp1 = rnd.randint(0, len(players1))
-    ##print "mvp1", mvp1
     mvp2 =rnd.randint(0, len(players2))
-    ##print "mvp2", mvp2
 
-    ##print "score1", score1
-    ##print "score2", score2
 
     # updating player stats
     score1 = updatePlayerStats(players1)
@@ -161,29 +132,39 @@ for matchId in range(1, matchCount+1):
         win2 = 1
         loss1 = 1
 
+    rating1 = team1['stats']['rating'];
+    rating2 = team2['stats']['rating'];
+    print rating1
+    print rating2
+    expect1 = 1.0 / (1.0 + pow(10, (1.0 * (rating2 - rating1) / 400)))
+    expect2 = 1.0 / (1.0 + pow(10, (1.0 * (rating1 - rating2) / 400)))
+    print expect1
+    print expect2
+
+    rating1 = rating1 + 20 * (win1 - expect1)
+    rating2 = rating2 + 20 * (win2 - expect2)
+    print rating1
+    print rating2
+    print "***************************"
+
     win1 = team1['stats']['won'] + win1;
-    print team1
-    print '------------win1', win1
     lost1 = team1['stats']['lost'] + loss1;
 
     win2 = team2['stats']['won'] + win2;
-    print team1
-    print '------------win1', win1
     lost2 = team2['stats']['lost'] + loss2;
 
-    print "team1Id", team1Id
     result = db.teamprofiles.update_one(
         { "teamId": str(team1Id) },
         {
-            "$set": {"stats.won":win1, "stats.lost":lost1}
+            "$set": {"stats.won":win1, "stats.lost":lost1, "stats.rating":rating1}
         }
     )
-    print '-----------', result.matched_count
-    print '-----------', result.modified_count
+    print result.matched_count
+    print result.modified_count
     result = db.teamprofiles.update_one(
         { "teamId": str(team2Id) },
         {
-            "$set": {"stats.won":win2, "stats.lost":lost2}
+            "$set": {"stats.won":win2, "stats.lost":lost2, "stats.rating":rating2}
         }
     )
 
@@ -200,5 +181,4 @@ for matchId in range(1, matchCount+1):
                 }
             )
 
-    ##print result
 

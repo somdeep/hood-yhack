6151a5602930b02a313969cded14ba8eeb3a302e
somdeep <sd2988@columbia.edu>
2015-11-07 21:24:42 -0500

    Profile from Db

--- public/scripts/controller/profileCtrl.js
+++ public/scripts/controller/profileCtrl.js
@@ -9,4 +9,13 @@ app.controller('profileCtrl', function ($scope,$http,$location, $routeParams) {
     .error(function(err) {
         console.log(err);
     });
+
+		$http.get('/playerStats/get/' + $routeParams.pId)
+    .success(function(data) {
+        $scope.currentUserStats = data[0];
+        console.log('******',data);
+    })
+    .error(function(err) {
+        console.log(err);
+    });
 });

8237ac41dfff4fbe1006eb15770205e96aabe14d
Devwrat More <devwratmmore@gmail.com>
2015-11-08 08:00:03 -0500

    changing server.js to remove mongoose

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ var bodyParser     = require('body-parser');			// To fetch data during posts
 
 
 var port  	 = process.env.PORT || 8000; 				// set the port
-var mongoose = require('mongoose'); 					// mongoose for mongodb
 var bodyParser     = require('body-parser');			// To fetch data during posts
 var port  	 = process.env.PORT || 8000; 				// set the port
-var database = require('./app/config/database'); 			// load the database config
-
-var db = mongoose.connect(database.url);	// connect to mongoDB database on modulus.io
 // auth related
 var session      = require('express-session');
 

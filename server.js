c52e3f10790bf60e1c8c7b44324ffc048bad0502
Devwrat More <devwratmmore@gmail.com>
2015-11-07 19:20:56 -0500

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Hello World\n');
-}).listen(port);
+
+//Initial configuration
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+
+var port  	 = process.env.PORT || 8000; 				// set the port
+
+app.use(express.static(__dirname + '/public')); 	// set the static files location
+
+require('./app/routes/routes.js')(app);
+
+
+//Start the awesomeness
+app.listen(port);	
+console.log('Magic happens on port '+port); 

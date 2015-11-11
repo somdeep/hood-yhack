80c98e55d2c7232cb29f747bf29b1e64083af350
somdeep <sd2988@columbia.edu>
2015-11-11 16:56:12 -0500

    minor changes to front end

--- public/scripts/controller/profileCtrl.js
+++ public/scripts/controller/profileCtrl.js
@@ -5,17 +5,11 @@ app.controller('profileCtrl', function ($scope,$http,$location, $routeParams) {
     .success(function(data) {
         $scope.currentUser = data[0];
         console.log('******',data);
+
     })
     .error(function(err) {
         console.log(err);
     });
 
-		$http.get('/playerStats/get/' + $routeParams.pId)
-    .success(function(data) {
-        $scope.currentUserStats = data[0];
-        console.log('******',data);
-    })
-    .error(function(err) {
-        console.log(err);
-    });
+
 });
